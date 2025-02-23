35e88416d73b100819d2169d2126a874cc34db11
guillermo-navas-palencia <g.navas.palencia@gmail.com>
2025-02-23 14:37:44 +0100

    NIG CDF backup

--- code/src/nig.cpp
+++ code/src/nig.cpp
@@ -1,3 +1,12 @@
+/*  Normal Inverse Gaussian cumulative distribution function.
+ *  
+ *  Implementation combines two special cases and the general case.
+ *  Use numerical integration as a backup.
+ * 
+ *  Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
+ *  Copyright (C) 2024
+ */
+
 #include <nig.hpp>
 
 
@@ -9,10 +18,17 @@ double nig_cdf(
   const double delta
 )
 {
+  double cdf;
+
   if (x == mu)
-    return nig_x_eq_mu(alpha, beta, delta);
+    cdf = nig_x_eq_mu(alpha, beta, delta);
   else if (beta == 0.0)
-    return nig_beta_eq_zero(x, alpha, mu, delta);
+    cdf = nig_beta_eq_zero(x, alpha, mu, delta);
   else
-    return nig_general(x, alpha, beta, mu, delta);
+    cdf = nig_general(x, alpha, beta, mu, delta);
+
+  if (cdf == -1.0)
+    return nig_integration(x, alpha, beta, mu, delta, 1e-13, 14);
+  else
+    return cdf;
 }
